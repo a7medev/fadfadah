f15b6556ba3c945ea2515d069422b498bae4478b
Ahmed Mahmoud <a7med.mahmoud2004@gmail.com>
2020-08-08 12:29:48 +0200

    refactored login code

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -10,13 +10,16 @@ const Login = () => {
 
   function handleLogin(event: FormEvent) {
     event.preventDefault();
-    auth.signInWithEmailAndPassword(email.current?.value!, password.current?.value!);
+    auth.signInWithEmailAndPassword(
+      email.current?.value!,
+      password.current?.value!
+    );
   }
 
   return (
     <PageTransition>
       <Container>
-        <Card style={{ maxWidth: '600px' }} className='mx-auto my-3'>
+        <Card style={{ maxWidth: '600px' }} className="mx-auto my-3">
           <Card.Body>
             <Card.Title className="text-center">
               <h3>تسجيل الدخول</h3>
@@ -25,21 +28,28 @@ const Login = () => {
             <form onSubmit={handleLogin}>
               <Form.Group controlId="email">
                 <Form.Label>البريد الإلكتروني</Form.Label>
-                <Form.Control ref={email} type="email" placeholder="اكتب بريد حسابك الإلكتروني" />
+                <Form.Control
+                  ref={email}
+                  type="email"
+                  placeholder="اكتب بريد حسابك الإلكتروني"
+                />
               </Form.Group>
               <Form.Group controlId="password">
                 <Form.Label>كلمة المرور</Form.Label>
-                <Form.Control ref={password} type="password" placeholder="اكتب كلمة مرور حسابك" />
+                <Form.Control
+                  ref={password}
+                  type="password"
+                  placeholder="اكتب كلمة مرور حسابك"
+                />
               </Form.Group>
 
               <Button type="submit">تسجيل الدخول</Button>
             </form>
-
           </Card.Body>
         </Card>
       </Container>
     </PageTransition>
   );
-}
+};
 
 export default Login;
