683adec2a229b6b9f5cb1244cf47d955b9c012d2
Ahmed Mahmoud <a7med.mahmoud2004@gmail.com>
2020-08-12 17:37:14 +0200

    redirection user to home page_ if profile username is his username + added page transition + handled not found user profile

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,13 +1,17 @@
 import * as React from 'react';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useContext } from 'react';
 import { RouteComponentProps } from 'react-router-dom';
-import { Container, Alert } from 'react-bootstrap';
+import { Container, Alert, Button } from 'react-bootstrap';
 import { functions } from '../config/firebase';
 import Loader from '../components/Loader';
 import { motion } from 'framer-motion';
 import UserCard from '../components/UserCard';
 import SendMessage from '../components/SendMessage';
 import MiniUser from '../types/MiniUser';
+import { AuthContext } from '../store/AuthContext';
+import { LinkContainer } from 'react-router-bootstrap';
+import { BsArrowRightShort } from 'react-icons/bs';
+import PageTransition from '../components/PageTransition';
 
 export interface ProfileProps
   extends RouteComponentProps<{ username: string }> {}
@@ -15,13 +19,16 @@ export interface ProfileProps
 const Profile: React.FC<ProfileProps> = ({
   match: {
     params: { username }
-  }
+  },
+  history
 }) => {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
   const [user, setUser] = useState<MiniUser | null>(null);
+  const { username: currentUsername } = useContext(AuthContext)!;
 
   useEffect(() => {
+    if (username !== currentUsername) {
       const getUserByUsername = functions.httpsCallable('getUserByUsername');
       getUserByUsername(username)
         .then(result => {
@@ -31,26 +38,43 @@ const Profile: React.FC<ProfileProps> = ({
         })
         .catch(err => {
           setLoading(false);
-          setError('حدثت مشكلة في الشبكة، تأكد من اتصال الإنترنت لديك')
+          setError('حدثت مشكلة في الشبكة، تأكد من اتصال الإنترنت لديك');
         });
-  }, [username]);
+    } else history.replace('/home');
+  }, [username, currentUsername, history]);
 
   return (
-    <Container>
-      {loading ? (
-        <Loader style={{ marginTop: 150, marginBottom: 20 }} />
-      ) : error ? (
-        <Alert variant="danger" onClose={() => setError(null)} dismissible>
-          {error}
-        </Alert>
-      ) : user && (
-        <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
-          <UserCard user={user} username={username} />
+    <PageTransition>
+      <Container>
+        {loading ? (
+          <Loader style={{ marginTop: 150, marginBottom: 20 }} />
+        ) : error ? (
+          <Alert variant="danger" onClose={() => setError(null)} dismissible>
+            {error}
+          </Alert>
+        ) : user ? (
+          <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
+            <UserCard user={user} username={username} />
 
-          <SendMessage user={user} />
-        </motion.div>
-      )}
-    </Container>
+            <SendMessage user={user} />
+          </motion.div>
+        ) : (
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            className="text-center"
+          >
+            <p className="text-muted h2 mt-5 mb-3">هذا المستخدم غير موجود</p>
+            <LinkContainer to="/home">
+              <Button>
+                <BsArrowRightShort className="ml-1" size="1.5em" />
+                عودة إلى الرئيسية
+              </Button>
+            </LinkContainer>
+          </motion.div>
+        )}
+      </Container>
+    </PageTransition>
   );
 };
 
