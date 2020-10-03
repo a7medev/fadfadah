88f6c158ae539d6ef8a00797c4ed36be7cd3d7f1
Ahmed Mahmoud <a7med.mahmoud2004@gmail.com>
2020-10-03 07:17:33 +0200

    Changed useInbox to use the getInbox function

--- src/hooks/useInbox.ts
+++ src/hooks/useInbox.ts
@@ -1,97 +1,49 @@
 import { useState, useEffect, useRef } from 'react';
-import { db } from '../config/firebase';
+import {  functions } from '../config/firebase';
 import Message from '../types/Message';
-import { Timestamp, DocumentData } from '@firebase/firestore-types';
 
-function useInbox(
-  userId?: string | null
-): [Message<Timestamp>[], () => void, boolean, boolean, boolean, Error | null] {
-  const [inbox, setInbox] = useState<Message<Timestamp>[]>([]);
+const getInbox = functions.httpsCallable('getInbox');
+
+function useInbox(): [
+  Message<Date>[],
+  () => void,
+  boolean,
+  boolean,
+  boolean,
+  Error | null
+] {
+  const [inbox, setInbox] = useState<Message<Date>[]>([]);
   const [hasMore, setHasMore] = useState(true);
   const [loadingMore, setLoadingMore] = useState(false);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<Error | null>(null);
 
-  const last = useRef<DocumentData>();
-
-  const ref = db
-    .collection('messages')
-    .where('to', '==', userId)
-    .orderBy('createdAt', 'desc')
+  const last = useRef<string>();
 
   useEffect(() => {
-    if (!userId) {
-      setInbox([]);
-      setLoading(false);
-      setError(new Error('user id is not set'));
-      return;
-    }
-
-    ref
-      .limit(12)
-      .get()
-      .then(snap => {
-        last.current = snap.docs[snap.docs.length - 1];
-
-        if (snap.docs.length < 12) setHasMore(false);
-
-        setInbox(
-          snap.docs.map(doc => ({
-            id: doc.id,
-            ...(doc.data() as Message<Timestamp>)
-          }))
-        );
-
-        setLoading(false);
-        setError(null);
+    getInbox()
+      .then(({ data: inbox }) => {
+        last.current = inbox[inbox.length - 1].id;
+        if (inbox.length < 8) setHasMore(false);
+        setInbox(inbox);
+        console.log(inbox);
       })
-      .catch(err => {
-        setLoading(false);
-        setError(err);
-      });
-
-    const unsub = ref.onSnapshot(snap => {
-      const changes = snap.docChanges();
-
-      const deleted = changes.filter(change => change.type === 'removed');
-
-      deleted.forEach(snap => {
-        setInbox(prevInbox =>
-          prevInbox.filter(message => message.id !== snap.doc.id)
-        );
-      });
-    });
-
-    return () => unsub();
+      .catch(err => setError(err))
+      .finally(() => setLoading(false));
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   function loadMore() {
     setLoadingMore(true);
 
-    ref
-      .limit(12)
-      .startAfter(last.current)
-      .get()
-      .then(snap => {
-        last.current = snap.docs[snap.docs.length - 1];
-
-        if (snap.docs.length < 12) setHasMore(false);
-
-        setInbox(prevInbox => {
-
-          return [
-            ...prevInbox,
-            ...snap.docs.map(doc => ({
-              id: doc.id,
-              ...(doc.data() as Message<Timestamp>)
-            }))
-          ];
-        });
-
-        setLoadingMore(false);
+    getInbox({ last: last.current })
+      .then(({ data: inbox }) => {
+        last.current = inbox[inbox - 1];
+        if (inbox.length < 8) setHasMore(false);
+        setInbox(prevInbox => [...prevInbox, ...inbox]);
       })
-      .catch(err => setError(err));
+      .catch(err => setError(err))
+      .finally(() => setLoadingMore(false));
   }
 
   return [inbox, loadMore, hasMore, loadingMore, loading, error];
