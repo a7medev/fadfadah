a55699369d8fb4d15bcc1b55e87dbaf00af367b3
Ahmed Mahmoud <a7med.mahmoud2004@gmail.com>
2020-10-06 07:22:58 +0200

    Changed the look of Who request

--- src/components/WhoRequest.tsx
+++ src/components/WhoRequest.tsx
@@ -1,28 +1,32 @@
 import * as React from 'react';
 import { Button, Card } from 'react-bootstrap';
 import IWhoRequest from '../types/WhoRequest';
-import UserData from './UserData';
-import { Emojione as Emoji } from 'react-emoji-render';
+import UserPhoto from './UserPhoto';
 
 export interface WhoRequestProps extends IWhoRequest {}
 
 const WhoRequest: React.FC<WhoRequestProps> = ({ message, from }) => {
   return (
     <Card>
-      <Card.Body className="py-2">
-        <UserData user={from} />
-        <hr className="mt-2" />
+      <Card.Body className="d-flex flex-wrap py-3">
+        <div className="d-flex flex-grow-1">
+          <UserPhoto url={from.photoURL} displayName={from.displayName} size={40} />
 
-        <p>
-          <small className="mb-1 text-muted d-block">على الرسالة</small>
-          <Emoji text={message.content} />
-        </p>
+          <div className="flex-grow-1 mr-2">
+            <p className="text-muted mb-n1" style={{ fontSize: '0.9rem' }}>يريد {from.displayName ?? 'مستخدم فضفضة'} أن يعرف من تكون على الرسالة</p>
+            <p className="mb-0">
+              {message.content}
+            </p>
+          </div>
+        </div>
 
-        <hr className="mb-2" />
-
-        <div className="d-flex justify-content-end">
-          <Button className="ml-2">قبول</Button>
-          <Button variant="text-dark">رفض</Button>
+        <div className="mt-1 align-self-end mr-auto">
+          <Button className="ml-1" size="sm">
+            قبول
+          </Button>
+          <Button variant="text-dark" size="sm">
+            رفض
+          </Button>
         </div>
       </Card.Body>
     </Card>
