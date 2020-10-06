ff3c6b3c19408fbbb88cd54377ef43c18b69aec7
Ahmed Mahmoud <a7med.mahmoud2004@gmail.com>
2020-10-06 17:36:08 +0200

    Message box hides after 5 seconds

--- src/components/MessageBox.tsx
+++ src/components/MessageBox.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { useState, useRef, useEffect } from 'react';
 import { createPortal } from 'react-dom';
 import { Modal, Button } from 'react-bootstrap';
 import './MessageBox.scss';
@@ -10,15 +11,39 @@ export interface MessageBoxProps {
   onClose: (event: React.MouseEvent<HTMLElement, MouseEvent>) => void;
 }
 
-const MessageBox: React.FC<MessageBoxProps> = ({ title, text, show, onClose }) => {
+const MessageBox: React.FC<MessageBoxProps> = ({
+  title,
+  text,
+  show,
+  onClose
+}) => {
+  const [showBox, setShowBox] = useState<boolean>(false);
+
+  const timeout = useRef<NodeJS.Timeout>();
+
+  useEffect(() => {
+    setShowBox(show);
+
+    if (show) {
+      timeout.current = setTimeout(() => {
+        setShowBox(false);
+      }, 5000);
+    } else if (timeout.current) {
+      clearTimeout(timeout.current);
+      timeout.current = undefined;
+    }
+  }, [show]);
+
   return createPortal(
-    <div className={'modal-content message-box ' + (show ? 'show' : '')}>
+    <div className={'modal-content message-box ' + (showBox ? 'show' : '')}>
       <Modal.Header>
         <Modal.Title>{title}</Modal.Title>
       </Modal.Header>
       <Modal.Body className="py-4 text-center">{text}</Modal.Body>
       <Modal.Footer>
-        <Button variant="text-dark" size="sm" onClick={onClose}>إغلاق</Button>
+        <Button variant="text-dark" size="sm" onClick={onClose}>
+          إغلاق
+        </Button>
       </Modal.Footer>
     </div>,
     document.getElementById('message')!
