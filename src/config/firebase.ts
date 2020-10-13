6429229febd429e92044253d16f8b1f92c3d8e84
Ahmed Mahmoud <a7med.mahmoud2004@gmail.com>
2020-10-13 06:53:26 +0200

    Changed config to use env vars

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -8,14 +8,14 @@ import 'firebase/analytics';
 import 'firebase/performance';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyB9QiKhNpNd0-FXrxSlYShusU_95quET8I',
-  authDomain: 'auth.fadfadah.me',
-  databaseURL: 'https://fad-fadah.firebaseio.com',
-  projectId: 'fad-fadah',
-  storageBucket: 'fad-fadah.appspot.com',
-  messagingSenderId: '514590012549',
-  appId: '1:514590012549:web:d679963f9c71faebba65ec',
-  measurementId: 'G-L6L6JWXE14'
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
