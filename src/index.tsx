a4f66d283a7d71fee515e6109e2db4d8cbdc5158
Ahmed Mahmoud <a7med.mahmoud2004@gmail.com>
2020-10-27 12:37:07 +0200

    Returned the app as it was

--- src/index.tsx
+++ src/index.tsx
@@ -1,31 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import App from './App';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
-// import AuthContextProvider from './contexts/AuthContext';
+import AuthContextProvider from './contexts/AuthContext';
 import './assets/scss/main.scss';
-// import 'moment/locale/ar';
+import 'moment/locale/ar';
 
 ReactDOM.render(
-  <div
-    className="bg-primary text-white d-flex justify-content-center align-items-center flex-column p-5"
-    style={{
-      height: '100vh',
-      width: '100%'
-    }}
-  >
-    <h1>سنعود قريبًا.</h1>
-    <p className="lead">
-      نأسف على إخباركم أن فضفضة توقف عن العمل لفترة قصيرة، وسنعلمكم عندما نعود.
-    </p>
-    <p className="text-left w-100" style={{ opacity: 0.8 }}>
-      {' '}
-      - مع تحيات فريق فضفضه
-    </p>
-    {/* <AuthContextProvider>
+  <AuthContextProvider>
     <App />
-  </AuthContextProvider> */}
-  </div>,
+  </AuthContextProvider>,
   document.getElementById('root')
 );
 
