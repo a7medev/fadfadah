32c435a188b03434fc10a3c3a330cc9f9f4c6d58
Ahmed Mahmoud <a7med.mahmoud2004@gmail.com>
2021-04-17 00:23:28 +0200

    chore(website): lazy load routes

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -1,27 +1,25 @@
 import React from 'react';
-
 import { Router } from '@reach/router';
 import { AnimatePresence } from 'framer-motion';
-
-import { useAuth } from './contexts/AuthContext';
+import loadable from '@loadable/component';
 
 import SafeArea from './components/SafeArea';
 import Navigation from './components/Navbar';
 import BottomNavigation from './components/BottomNavbar';
-
-import Home from './pages/Home';
-import Login from './pages/auth/Login';
-import Register from './pages/auth/Register';
-import ResetPassword from './pages/auth/ResetPassword';
-import Profile from './pages/Profile';
-import PrivacyPolicy from './pages/PrivacyPolicy';
-import Settings from './pages/signed/Settings';
-import Outbox from './pages/signed/Outbox';
-import Inbox from './pages/signed/Inbox';
-import NotFound from './pages/NotFound';
-import WhoRequests from './pages/signed/WhoRequests';
-
 import useDarkMode from './hooks/useDarkMode';
+import { useAuth } from './contexts/AuthContext';
+
+const Home = loadable(() => import('./pages/Home'));
+const Login = loadable(() => import('./pages/auth/Login'));
+const Register = loadable(() => import('./pages/auth/Register'));
+const ResetPassword = loadable(() => import('./pages/auth/ResetPassword'));
+const Profile = loadable(() => import('./pages/Profile'));
+const PrivacyPolicy = loadable(() => import('./pages/PrivacyPolicy'));
+const Settings = loadable(() => import('./pages/signed/Settings'));
+const Outbox = loadable(() => import('./pages/signed/Outbox'));
+const Inbox = loadable(() => import('./pages/signed/Inbox'));
+const NotFound = loadable(() => import('./pages/NotFound'));
+const WhoRequests = loadable(() => import('./pages/signed/WhoRequests'));
 
 const App: React.FC = () => {
   useDarkMode();
