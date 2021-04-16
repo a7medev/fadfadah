5762df40c9f049dda36fed40942b6bf656185e02
Ahmed Mahmoud <a7med.mahmoud2004@gmail.com>
2021-04-17 01:48:27 +0200

    chore(website): use new icons in bottom nav

--- website/src/components/BottomNavbar.tsx
+++ website/src/components/BottomNavbar.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import { Navbar } from 'react-bootstrap';
-import { FaPaperPlane, FaCog, FaInbox } from 'react-icons/fa';
+import InboxIcon from './icons/InboxIcon';
+
+import InboxOutlinedIcon from './icons/InboxOutlinedIcon';
+import OutboxIcon from './icons/OutboxIcon';
+import OutboxOutlinedIcon from './icons/OutboxOutlinedIcon';
+import SettingsIcon from './icons/SettingsIcon';
+import SettingsOutlinedIcon from './icons/SettingsOutlinedIcon';
 import Link from './Link';
 
 const BottomNavigation: React.FC = () => {
@@ -17,7 +23,8 @@ const BottomNavigation: React.FC = () => {
         className="bottom-nav-icon"
         aria-label="الرسائل المرسلة"
       >
-        <FaPaperPlane size="25" />
+        <OutboxIcon className="filled" size={25} />
+        <OutboxOutlinedIcon className="outlined" size={25} />
       </Link>
 
       <Link
@@ -25,11 +32,13 @@ const BottomNavigation: React.FC = () => {
         className="bottom-nav-icon"
         aria-label="الرسائل المستلمة"
       >
-        <FaInbox size="25" />
+        <InboxIcon className="filled" size={25} />
+        <InboxOutlinedIcon className="outlined" size={25} />
       </Link>
 
       <Link to="/settings" className="bottom-nav-icon" aria-label="الإعدادات">
-        <FaCog size="25" />
+        <SettingsIcon className="filled" size={25} />
+        <SettingsOutlinedIcon className="outlined" size={25} />
       </Link>
     </Navbar>
   );
