9789048658e40c3c56768bb85e844c9ea436b842
Ahmed Mahmoud <a7med.mahmoud2004@gmail.com>
2021-04-17 23:45:29 +0200

    fix(website/pwa): register service worker

--- website/src/index.tsx
+++ website/src/index.tsx
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import AuthProvider from './contexts/AuthContext';
 import './assets/scss/main.scss';
 import 'moment/locale/ar';
@@ -12,7 +12,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+serviceWorkerRegistration.register();
