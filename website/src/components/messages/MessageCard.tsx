74566054430fd08a90dd7ed8a5b21d545ea85044
Ahmed Mahmoud <a7med.mahmoud2004@gmail.com>
2021-05-03 22:27:24 +0200

    chore(website): create message container component

--- website/src/components/messages/MessageCard.tsx
+++ website/src/components/messages/MessageCard.tsx
@@ -1,11 +1,8 @@
-import { Card } from 'react-bootstrap';
 import { Emojione as Emoji } from 'react-emoji-render';
 import type { Timestamp } from '@firebase/firestore-types';
 
 import type Message from '../../types/Message';
-import MessageHeader from './MessageHeader';
-import MessageFooter from './MessageFooter';
-import FadeTransition from '../FadeTransition';
+import MessageContainer from './MessageContainer';
 import styles from './MessageCard.module.css';
 
 export interface MessageCardProps {
@@ -20,23 +17,11 @@ const MessageCard: React.FC<MessageCardProps> = ({
   onDelete
 }) => {
   return (
-    <FadeTransition>
-      <Card className="mb-3" id={message.id}>
-        <Card.Body className={`pb-2 ${message.from || outbox ? 'pt-2' : ''}`}>
-          <MessageHeader message={message} outbox={outbox} />
-
-          <p className={styles.messageText}>
-            <Emoji text={message.content} />
-          </p>
-
-          <MessageFooter
-            message={message}
-            outbox={outbox}
-            onDelete={onDelete}
-          />
-        </Card.Body>
-      </Card>
-    </FadeTransition>
+    <MessageContainer message={message} outbox={outbox} onDelete={onDelete}>
+      <p className={styles.messageText}>
+        <Emoji text={message.content} />
+      </p>
+    </MessageContainer>
   );
 };
 
---
+++ website/src/components/messages/MessageContainer.tsx
@@ -0,0 +1,40 @@
+import { Card } from 'react-bootstrap';
+import type { Timestamp } from '@firebase/firestore-types';
+
+import type Message from '../../types/Message';
+import MessageHeader from './MessageHeader';
+import MessageFooter from './MessageFooter';
+import FadeTransition from '../FadeTransition';
+
+export interface MessageContainerProps {
+  message: Message<Timestamp>;
+  outbox?: boolean;
+  onDelete: (id: string) => void;
+}
+
+const MessageContainer: React.FC<MessageContainerProps> = ({
+  children,
+  message,
+  outbox,
+  onDelete
+}) => {
+  return (
+    <FadeTransition>
+      <Card className="mb-3" id={message.id}>
+        <Card.Body className={`pb-2 ${message.from || outbox ? 'pt-2' : ''}`}>
+          <MessageHeader message={message} outbox={outbox} />
+
+          {children}
+
+          <MessageFooter
+            message={message}
+            outbox={outbox}
+            onDelete={onDelete}
+          />
+        </Card.Body>
+      </Card>
+    </FadeTransition>
+  );
+};
+
+export default MessageContainer;
